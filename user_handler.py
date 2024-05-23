b9cb1b5818e3b8c1ac918d19ec41c19a4cb8fc74
MitchellWillis05 <mitchellwillis33@gmail.com>
2024-05-23 21:27:00 +1200

    added user_database.db, user_handler.py, main.py, templates folder, base.html, home.html, sign_up.html, static fodler, style.css

---
+++ user_handler.py
@@ -0,0 +1,11 @@
+import sqlite3
+
+
+def db_connect():
+    conn = None
+    try:
+        conn = sqlite3.connect("user_database.db")
+    except sqlite3.Error as error:
+        print(error.sqlite_errorcode)
+        print(error.sqlite_errorname)
+    return conn
